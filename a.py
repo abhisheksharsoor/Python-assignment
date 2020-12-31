8036b28c745d7b26ee7a20e984a727f1a03bbda9
abhisheksharsoor <53474203+abhisheksharsoor@users.noreply.github.com>
2020-12-31 23:51:25 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,14 @@
+import os
+
+
+
+allfiles = os.listdir()
+
+for i in allfiles:
+    if i == "a.py" or i == "create.py":
+        continue
+
+    newlist = i.split("-")
+    newname = newlist[1]+"-"+newlist[0]+"-"+newlist[2]
+    os.rename(i,newname) 
+    
